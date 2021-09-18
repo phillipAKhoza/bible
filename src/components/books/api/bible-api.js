d84e94db4098a2b2cfb8dcff123d322088ef9db0
Phillip khoza <phillipa.khoza@itthynk.co.za>
2021-09-18 02:56:22 +0200

    chapter api

--- src/components/books/api/bible-api.js
+++ src/components/books/api/bible-api.js
@@ -4,34 +4,6 @@ import books from './Bible-books.json';
 
 export class Bible extends Component {
  
-
-    // if(test === "New Testament"){       
-    //   for(i =0; i < new_test[place]; i++){
-    //     capts[i] = i + 1;
-    //   }
-        
-    // }else if( test === "Old Testament"){
-    //   for(i =0; i < old_test[place]; i++){
-    //     capts[i] = i + 1;
-    //   } 
-    // }
-
-    // var books = require('./books.json');
-    // const new_Test_Books = books.The_New_Testament;
-    // const old_Test_Books = books.The_Old_Testament;
-    // const new_Testament = new_Test_Books.split(",");
-    // const old_Testament = old_Test_Books.split(",");
-    // const DATA = [
-    //   {
-    //     title: "Old Testament",
-    //     data: old_Testament,
-    //   },
-    //   {
-    //     title: "New Testament",
-    //     data: new_Testament,
-    //   },
-    // ];
-
     render() {
         return (       
           <div className="site-main">
