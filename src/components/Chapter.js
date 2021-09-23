bd26be1e3ac6f54e91527ea7268546751216fccc
Phillip khoza <phillipa.khoza@itthynk.co.za>
2021-09-23 15:29:58 +0200

    idk

--- src/components/Chapter.js
+++ src/components/Chapter.js
@@ -6,21 +6,70 @@ export default class Chapter extends Component {
     constructor(props) {
         super(props);
         this.state = {
-        place: this.props.location.chapterProps.place,
-        book: this.props.location.chapterProps.book,
+        place: -1,
+        book: undefined,
         book_chapters:[],
+        hasPassedValues: true,
         };
-        // console.log(this.state.place);
+        try{
+
+        this.state.place= this.props.location.chapterProps.place;
+        this.state.book= this.props.location.chapterProps.book;
+        console.log(this.state.place);
         console.log(this.state.book+" has "+chapters[this.state.place]+" chapters");
-        for(let i = 0; i< chapters[this.state.place]; i++){
-            // this.setState({
-            this.state.book_chapters[i]= i +1;
-            // });
+        if(this.state.place !== -1){
+            for(let i = 0; i< chapters[this.state.place]; i++){
+                // this.setState({
+                this.state.book_chapters[i]= i +1;
+                // });
+            }
+        }else{
+            this.state.hasPassedValues = false;
+            for(let i = 0; i< chapters[0]; i++){
+                // this.setState({
+                this.state.book_chapters[i]= i +1;
+                // });
+            }
+            console.log(this.state.place);
+            console.log(this.state.book+" has "+chapters[this.state.place]+" chapters");
         }
+        }catch(e){
+            // console.log(e);
+        }
+    }
+    withChapter(){
+        this.setState({ 
+             place: this.props.location.chapterProps.place,
+             book: this.props.location.chapterProps.book,
+          });
+          const book_chapter =[];
+          for(let i = 0; i< chapters[this.state.place]; i++){
+                // this.setState({
+                book_chapter[i]= i +1;
+                // });
+            } 
+        this.setState({ 
+             book_chapters: book_chapter,
+          });   
+    }
+    withoutChapters(){
+        
+         this.setState({ 
+             place: 0,
+             book: "Genesis",
+          });
+          const book_chapter =[];
+          for(let i = 0; i< chapters[this.state.place]; i++){
+                // this.setState({
+                book_chapter[i]= i +1;
+                // });
+            } 
+        this.setState({ 
+             book_chapters: book_chapter,
+          });   
     }
     
     render() {
-        
         return (       
           <div className="site-main">
             <div className="pk-container">
@@ -28,15 +77,26 @@ export default class Chapter extends Component {
                     <Header/>
                 </div>
                 <div className="chapter-container">
-                    { 
-                        this.state.book_chapters.map(((item,index) => {
-                        return(
-                        <div key={index} className="chapters" onClick={()=> {
-                            this.handleClick(item, index)   
-                        }}>
-                            <h3>{item}</h3>
-                        </div>
-                    )}))
+                    {
+                        this.state.hasPassedValues?
+                            this.state.book_chapters.map(((item,index) => {
+                            return(
+                            <div key={index} className="chapters" onClick={()=> {
+                                this.handleClick(item, index)   
+                            }}>
+                                <h3>{item}</h3>
+                            </div>
+                            )}))
+                        :
+                            this.state.book_chapters.map(((item,index) => {
+                            return(
+                            <div key={index} className="chapters" onClick={()=> {
+                                this.handleClick(item, index)   
+                            }}>
+                                <h3>{item}</h3>
+                            </div>
+                            )}))
+
                     }
                 </div>
             </div>
